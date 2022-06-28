3b17aa953b7696e8fb4553e3e58c4ce5134fe860
yangyuhuan <526779045@qq.com>
2022-06-28 16:01:14 +0800

    feat:deep-readonly,tuple-to-union,chainable-options

--- type-challengs/medium-chainable-options/template.ts
+++ type-challengs/medium-chainable-options/template.ts
@@ -1,4 +1,4 @@
-type Chainable = {
-  option(key: string, value: any): any
-  get(): any
+type Chainable<T = {}> = {
+  option<K extends string, V = any>(key: K extends keyof T ? never : K, value: V): Chainable<T & {[O in K] : V}>
+  get(): T
 }
